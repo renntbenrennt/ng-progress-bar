152cf0cbb10a4f1f8cde5db2fed2b9250857c3a5
Benjamin <jijileeeeeee@gmail.com>
2019-12-09 01:59:27 +0800

    Fix what have left

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,40 +18,60 @@ export class AppComponent implements OnInit {
   flexBasisProgressObj = {
     limit: 50,
     progress: 0
+  };
+
+  test = {
+    t: 0
   }
 
   ngOnInit(): void {
     this.widthProgressing();
     this.flexBasisProgressing();
+
+    // function tt(test) {
+    //   setTimeout(() => {
+    //     if (test.t < 10) {
+    //       test.t += 1;
+    //       tt(test);
+    //     }
+    //   }, 1000);
+    // }
+
+    // console.log(tt(this.test));
   }
 
   timeoutFunc(limit, progress) {
     setTimeout(() => {
-      if (progress < limit) {
-        // this.time += 10;
-        this.timeoutFunc(limit, progress += 10);
-        console.log(progress);
+      if (this[progress] < limit) {
+        this[progress] += 10;
+        this.timeoutFunc(limit, progress);
       }
     }, 1000);
   }
 
-  timeoutFuncWithObj(progressObj) {
-    setTimeout(() => {
-      if (progressObj.progress < progressObj.limit) {
-        progressObj.progress += 10;
-        this.timeoutFuncWithObj(progressObj);
-        console.log(progressObj.progress);
-      }
-    }, 1000);
-  }  
+  /// todo:
+  /// find out why the value is calculated, seemingly,
+  /// twice...
+  // timeoutFuncWithObj(progressObj) {
+  //   setTimeout(() => {
+  //     if (progressObj.progress < progressObj.limit) {
+  //       console.log("this 1 === ", this);
+  //       progressObj.progress += 10;
+  //       console.log(progressObj.progress);
+  //       console.log("this 2 === ", this);
+  //       this.timeoutFuncWithObj(progressObj);
+  //       console.log("this 3 === ", this);
+  //     }
+  //   }, 1000);
+  // }
 
   widthProgressing() {
-    // this.timeoutFunc(160, this.widthProgress);
-    this.timeoutFuncWithObj(this.widthProgressObj);
+    this.timeoutFunc(160, 'widthProgress');
+    // this.timeoutFuncWithObj(this.widthProgressObj);
   }
 
   flexBasisProgressing() {
-    // this.timeoutFunc(50, this.flexBasisProgress);
-    this.timeoutFuncWithObj(this.flexBasisProgressObj);
+    this.timeoutFunc(90, 'flexBasisProgress');
+    // this.timeoutFuncWithObj(this.flexBasisProgressObj);
   }
 }
